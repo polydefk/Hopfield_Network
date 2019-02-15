035a37e1e8cb16c9e554901db900d0adf1da4b02
Leonidas Valavanis <valavanisleonidas@gmail.com>
2019-02-15 11:49:09 +0100

    * added asynchronous non random update * added calculate energy at each iteration

--- hopfield_network.py
+++ hopfield_network.py
@@ -6,7 +6,6 @@ import Utils
 class Hopfield(object):
     def __init__(self, memory_patterns, method='Batch'):
         self.memory_patterns = memory_patterns
-
         self.method = method
 
     def initialize_weights(self):
@@ -33,51 +32,55 @@ class Hopfield(object):
             self.weight_matrix = np.add(np.outer(np.transpose(pattern), pattern), self.weight_matrix)
         # np.fill_diagonal(self.weight_matrix, 0)
 
-    def recall(self, pattern, i=-1, n_iterations=None):
+    def recall(self, pattern, n_iterations=None, method=None, calculate_energy=False):
         """:returns the attractor if this pattern is attractor
             or the pattern that minimizes the error
             or nothing"""
 
-        # error = 1
         iter = 0
         update = []
 
         if n_iterations is None:
             n_iterations = int(np.log(self.weight_matrix.shape[0])) + 1
 
+        energy = np.zeros(n_iterations)
+
+        if method is not None:
+            self.method = method
+
         if self.method is 'Batch':
-            update = self.update_batch
+            update = self.update_synchronous_batch
 
         if self.method is "Random":
-            update = self.update_random
+            update = self.update_asynchronous_random
+
+        if self.method is "Async":
+            update = self.update_asynchronous
 
         while True:
 
             new_x = update(pattern)
 
-            # if i > 0:
-            #     error = np.sum(np.abs(np.subtract(new_x, self.memory_patterns[i])))
-
-            iter += 1
-            # print(iter)
-
-            if np.array_equal(pattern, new_x):
-                print('found attractor at iter : {0} '.format(iter))
-                return pattern
+            # if np.array_equal(pattern, new_x):
+                # print('found attractor at iter : {0} '.format(iter))
+                # return [pattern, energy]
 
             pattern = new_x.copy()
 
-            if iter % 100 == 0:
+            if calculate_energy:
+                energy[iter] = self.calculate_energy(pattern)
+
+            iter += 1
+            if iter % 150 == 0:
                 Utils.display_image(pattern, 'recalled picture at {0}th iteration'.format(iter))
 
             if iter == n_iterations:
                 break
 
-        return pattern
+        return [pattern, energy]
 
-        # print("End of recall epochs needed {}".format(iter))
 
-    def update_batch(self, x):
+    def update_synchronous_batch(self, x):
         """:returns"""
         result = np.dot(self.weight_matrix, x)
         result[result >= 0] = 1.
@@ -85,13 +88,24 @@ class Hopfield(object):
 
         return result
 
-    def update_random(self, x):
+    def update_asynchronous_random(self, x):
         result = np.zeros(len(x))
 
         order = np.arange(len(x))
         np.random.shuffle(order)
 
         for i in order:
+            value = np.dot(self.weight_matrix[i], x)
+            if value >= 0:
+                result[i] = 1
+            else:
+                result[i] = -1
+        return result
+
+    def update_asynchronous(self, x):
+        result = np.zeros(len(x))
+
+        for i in range(len(x)):
             value = np.dot(self.weight_matrix[i], x)
             if value >= 0:
                 result[i] = 1
